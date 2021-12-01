43468cfc9a31c2fae117f8384096ddb9a4326d6b
ajacks12 <58667794+ajacks12@users.noreply.github.com>
2021-12-01 21:27:27 +0000

    mark WIP dirs and add some timer fcns

---
+++ 003/timer.m
@@ -0,0 +1,7 @@
+timeStart = now();
+q003;
+timeEnd = now();
+duration = timeEnd - timeStart;
+dur_s  = 24 * 60 * 60 * duration
+dur_ms = dur_s * 1000
+dur_us = dur_ms * 1000
---
+++ 005_WIP/timer.m
@@ -0,0 +1,7 @@
+timeStart = now();
+q005_WIP;
+timeEnd = now();
+duration = timeEnd - timeStart;
+dur_s  = 24 * 60 * 60 * duration
+dur_ms = dur_s * 1000
+dur_us = dur_ms * 1000
